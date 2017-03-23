1ad46c9d2e10b1c4d4a606d34df8303cf1893a3a
Saul <saulbarral@gmail.com>
2017-03-23 01:31:39 +0100

    No need for Youtube-dl
    With this changes spotifyt no longer needs youtube-dl library and FFMPEG.

--- spotifyt/__main__.py
+++ spotifyt/__main__.py
@@ -1,91 +1,107 @@
-from __future__ import unicode_literals
 from spotipy.oauth2 import SpotifyClientCredentials
+from tqdm import tqdm
 
-import youtube_dl
+import requests
 import spotipy
-import spotipy.util as util
+import sys
 import urllib.request
 import urllib.parse
 import re
 import os
+import time
 
-#Luastan's - Spotify -> Youtube -> download -> MP3.
+#Retieves data from spotify playlist given playlist id & user witch comes form uri
+def get_playlist_tracks(uri):
+    #Spotify api auth
+    client_credentials_manager = SpotifyClientCredentials("2e51cd035c754f138d0a18b72e69d2b3", "c242f72478c84e4aac07018307780905")
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-os.system("clear")
+    #Username and id
+    username = uri.split(':')[2]
+    playlist_id = uri.split(':')[4]
 
-#Spotify API credentials ("Client ID", "Client Secret")
-#Aqui puedes conseguir tus credenciales: https://developer.spotify.com/my-applications
-client_credentials_manager = SpotifyClientCredentials("", "")
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-#----------
-#Busqueda del primer resultado de youtube dado un texto
-def yt_search(canciones): #YT_VIDEO_ID = yt_search, cantidad("Lo que quieras encontrar en Youtube")
-    z=[]
-    for i in range(0,len(canciones)):
-        print('\rBuscando cancion %s de %s' % (i+1, len(canciones)), sep=' ', end=15*' '+'\r', flush=True)
-        texto=canciones[i]
-        query_string = urllib.parse.urlencode({"search_query" : texto})
-        html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
-        enlasitos = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-        z.append("https://www.youtube.com/watch?v=" + enlasitos[0]) #Devuelve la URL del video
-    return z
-
-print("############################")
-print("# Luastan's Spotify -> MP3 #")
-print("############################")
-#------------Start
-#Ejemplo_URI = 'spotify:user:luastan:playlist:1yd6b5BkiCO2k6ULHfqz8M'
-uri = input('URI de tu lista de Spotify:\n')
-username = uri.split(':')[2]
-playlist_id = uri.split(':')[4]
-print("############################\nAccion en progreso:")
-print("Leyendo lista de Spotify.", sep=' ', end=15*' '+'\r', flush=True)
-#----------- Saca la info de las canciones
-def get_playlist_tracks(username,playlist_id):
+    #Retrieve data from playlist
     results = sp.user_playlist_tracks(username,playlist_id)
     tracks = results['items']
+
+    #THis loop is required to bypass 100 track per list limit
     while results['next']:
         results = sp.next(results)
         tracks.extend(results['items'])
     return tracks
-#--------Song list humanizer
-def humanizer_returnsx(resultados):
-    L=[]
-    tam = len(resultados)
+
+#get_playlist_id returns a big ass dictionary with a shit ton of unnecesary data
+def humanizer(results):
+    canciones=[] #List to save song names
+    tam = len(results)
     for i in range(0,tam):
-        name = resultados[i]['track']['name']
+        name = results[i]['track']['name']
         saltimbanquis=""
-        for m in range(0,len(resultados[i]['track']['artists'])):
-            saltimbanquis = saltimbanquis + " " + resultados[i]['track']['artists'][m]['name']
-        L.append(name + saltimbanquis)
-    print('\r -> Datos extraidos correctamente !', sep=' ', end=15*' '+'\r', flush=True)
+        for m in range(0,len(results[i]['track']['artists'])):
+            saltimbanquis = saltimbanquis + " " + results[i]['track']['artists'][m]['name']
+        canciones.append(name + saltimbanquis)
 
-    return L
-#formato de la lista  ['song1 artist1 artist1a', 'song2 artist2', 'song3 artist 3']
-spotify_list = humanizer_returnsx(get_playlist_tracks(username, playlist_id))
-link_list = yt_search(spotify_list)
-print('\r -> Busqueda completada !!', sep=' ', end=15*' '+'\r', flush=True)
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'writethumbnail': True,
-    'extract-audio': True,
-    'audio-format': 'mp3',
-    'quiet': True,
-    'embed-thumbnail': True,
-    'outtmpl': 'Descargas/'+'%(title)s.%(ext)s', #Carpeta de descarga por defecto esto es en la ruta donde ejecutas el programa/Descargas
-    'postprocessors': [
-        {'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192'},
-        {'key': 'EmbedThumbnail',},
-    ]} #Estas opciones no me aclaraba con ellas no me juzgueis
+        #
+    return canciones
 
-def descargar(links):
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        for i in range(0, len(links)):
-            print('\rDescargando cancion %s de %s' % (i+1, len(links)), sep=' ', end=15*' '+'\r', flush=True)
-            ydl.download([links[i]])
+def print_status(current, total):
+    print('\rDescargando cancion %s de %s' % (current, total), sep=' ', end=50*' '+'\r', flush=True)
 
-descargar(link_list)
-print("\r -> Descarga completada !!", sep=' ', end=15*' '+'\n', flush=True)
+
+#Searches stuff in youtube. Returns first search result in youtubeinmp3 direct link format
+# WHy only first search result what if it is a music video¿??¿?¿?¿ - > Search Forbbiden voices MArtin Garrix
+def yt_in_mp3_generator(canciones):
+    links=[]
+    for i in range(0,len(canciones)):
+        texto=canciones[i]
+        legit_links=[]
+        query_string = urllib.parse.urlencode({"search_query" : texto})
+        html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+        enlasitos = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+        links.append("https://www.youtubeinmp3.com/fetch/?video=https://www.youtube.com/watch?v=" + enlasitos[0])
+
+    return links
+
+
+#FUnction name is self-explanatory
+def downloader(song_names, links_youtubeinmp3, path):
+    for i in range(len(song_names)):
+        filename = path + song_names[i] + '.mp3'
+        if not os.path.isfile(filename): #if not os.path.isfile(PATH + song_names[i] + '.mp3'):
+            os.makedirs(os.path.dirname(filename), exist_ok = True)
+            with urllib.request.urlopen(links_youtubeinmp3[i]) as data:
+                with open(filename, "wb") as f:
+                    f.write(data.read())
+
+
+
+def progressive_downloader(song_names, path): #This function merges the link generator with the downloader.
+    is_bbc = len(song_names)                  #I want to add a continue progress thing but mayb later
+    for i in range(is_bbc):
+        filename = path + song_names[i] + '.mp3'
+
+        print_status(i + 1, is_bbc)
+
+        if not os.path.isfile(filename):
+            downloader([song_names[i]], yt_in_mp3_generator([song_names[i]]), path) #In older versions i used to do this in 2 steps
+            statinfo = os.stat(filename)
+            #random im not a direct link anymore bullshic bypass
+            while statinfo.st_size < 1000000: #Sometimes youtubeinmp3 has the wonderfull idea of changing direct links to randomnn webpages
+                os.remove(filename)
+                downloader([song_names[i]], yt_in_mp3_generator([song_names[i]]), path) #In older versions i used to do this in 2 steps
+                statinfo = os.stat(filename)
+
+
+#www.youtubeinmp3.com/fetch/?video=https://www.youtube.com/watch?v=Zv1QV6lrc_Y
+if __name__ == '__main__':
+    #Actual program
+    path = "./Descargas/"
+    if len(sys.argv)>1:
+        uri = sys.argv[1]
+        if len(sys.argv)>2:
+            path = sys.argv[2]
+    else:
+        uri = input('sportify URI_')
+    #ista_canciones = humanizer(get_playlist_tracks(uri))
+    progressive_downloader(lista_canciones, path)
+    print('\n\n')
